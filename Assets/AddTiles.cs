15a4666deb1485bf71322871d5b27124bd185308
Andres <fmendez1212@gmail.com>
2021-06-26 15:20:42 -0500

    programatically add button indexed listeners

--- Assets/AddTiles.cs
+++ Assets/AddTiles.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class AddTiles : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public Transform tilesContainers;
+    public GameObject tilePrefab;
+    private void Start()
     {
-        
+        var buttons = GameObject.FindGameObjectsWithTag(tag: "AddTileButton");
+        for (var i = 0; i < buttons.Length; i++)
+        {
+            var i1 = i;
+            buttons[i].GetComponent<Button>()?.onClick.AddListener(call: ()=> addTile(index: i1));
+        }
+    }
+
+    private void addTile(int index) {
+        Debug.Log ("You have clicked the button! " + index);
+      //  tilesContainers.GetChild(index)
+            
     }
 
     // Update is called once per frame
@@ -16,8 +31,4 @@ public class AddTiles : MonoBehaviour
        
     }
 
-    public void AddTile(int column)
-    {
-        Debug.Log(column);
-    }
 }
